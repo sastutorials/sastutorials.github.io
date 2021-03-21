e3c1578b82abc73041488e09e95a689a741d0a12
Edoardo-Bruffa <80956166+Edoardo-Bruffa@users.noreply.github.com>
2021-03-21 10:53:10 +0100

    Update accordion.js

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -3,6 +3,6 @@ function myFunction(id) {
     if (x.className.indexOf("w3-show") == -1) {
       x.className += " w3-show";
     } else { 
-      x.className = x.className.replace(" w3-show", "");
+      x.className = x.className.replace("w3-show", "");
     }
   }
