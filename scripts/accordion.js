9fb3ff8658704b15ab8648058b9a979e72399b4d
Edoardo-Bruffa <80956166+Edoardo-Bruffa@users.noreply.github.com>
2021-03-21 11:05:15 +0100

    Update accordion.js

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,6 +1,6 @@
 function myFunction(id) {
     var x = document.getElementById(id);
-    if (!x.className.contains("w3-show")) {
+    if (x.className.indexOf("w3-show")== -1) {
       x.className += " w3-show";
     } else { 
       x.className = x.className.replace("w3-show", "");
