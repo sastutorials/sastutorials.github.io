13475d7c971e31ac7ee4154eb29ce5d0c17b1971
Edoardo-Bruffa <bruffaedoardo@gmail.com>
2021-07-06 15:28:27 +0200

    PROVA

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -7,9 +7,13 @@ function myFunction(id) {
     }
   }
 
-  function myFunction() {
-    var copyText = document.getElementById("myInput");
-    copyText.select();
-    document.execCommand("Copy");
-    alert("Copied the text: " + copyText.value);
+  function copyFunction() {
+    const copyText = document.getElementById("myInput").textContent;
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy");
   }
+  
+  document.getElementById('button').addEventListener('click', copyFunction);
